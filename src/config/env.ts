9ed026be6f15e7dc136f1221d714f6f83bb224f4
Angel Alvarez <anrealba9@gmail.com>
2025-06-30 23:08:23 -0600

    feat: validating envs

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import functions from 'firebase-functions';
 config();
 
 export const env = {
-  jwtSecret: process.env?.JWT_SECRET ?? functions.config().jwt.secret,
+  jwtSecret: process.env?.JWT_SECRET ?? functions?.config()?.jwt?.secret ?? 'dev-secret',
   keyRoute: process.env?.KEYS_ROUTE ?? '',
   environment: process.env?.NODE_ENV ?? process.env?.ENVIRONMENT ?? 'dev'
 };
